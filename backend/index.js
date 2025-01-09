d20577498816f075d574885fca17a1942513c000
@kammopajhb024 <mmopanemoroka@gmail.com>
2025-01-09 17:55:57 +0200

    database, controllers, user model, routes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -6,6 +11,11 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use("/api/auth", authRoutes);
+
 app.listen(3000, () => {
+  connectDB();
   console.log("Server is running on port 3000");
 });
+
+// zKWcS9GywOaOrjMw
