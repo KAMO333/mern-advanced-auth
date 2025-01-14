1d36a5cdcd2c3e81be4b905cc588da9ebf0f67d7
@kammopajhb024 <mmopanemoroka@gmail.com>
2025-01-14 17:57:47 +0200

    email $ password

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { Mail, Lock, Loader } from "lucide-react";
 import { Link } from "react-router-dom";
+import Input from "../components/Input";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -24,6 +25,23 @@ const LoginPage = () => {
         <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
           Welcome Back
         </h2>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            icon={Mail}
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            icon={Lock}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </form>
       </div>
     </motion.div>
   );
