a568d3cd46637d8e79d86d6b0c66ac1fbb30cbd1
@kammopajhb024 <mmopanemoroka@gmail.com>
2025-01-14 18:26:58 +0200

    spinner

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -9,6 +9,8 @@ const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const isLoading = false;
+
   const handleLogin = async (e) => {
     e.preventDefault();
     await login(email, password);
@@ -56,8 +58,13 @@ const LoginPage = () => {
             whileTap={{ scale: 0.98 }}
             className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
             type="submit"
+            disabled={isLoading}
           >
-            Login
+            {isLoading ? (
+              <Loader className="w-6 h-6 animate-spin  mx-auto" />
+            ) : (
+              "Login"
+            )}
           </motion.button>
         </form>
       </div>
