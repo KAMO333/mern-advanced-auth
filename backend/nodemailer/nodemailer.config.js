399ffc1b98e3ad82d6dd34ade34fb0d9cae9c4b5
KAMO333 <mmopanemoroka@gmail.com>
2026-02-24 15:05:58 +0200

    Refactor Nodemailer configuration to specify SMTP host and port settings

--- backend/nodemailer/nodemailer.config.js
+++ backend/nodemailer/nodemailer.config.js
@@ -4,11 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
 });
 
 export const sender = {
