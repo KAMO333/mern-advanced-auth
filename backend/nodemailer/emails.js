de3739b83dc497dd3c3c6efd12722b0808de0bb6
KAMO333 <mmopanemoroka@gmail.com>
2026-02-24 14:42:05 +0200

    Refactor password reset and success email functions to use transporter instead of mailtrapClient

--- backend/nodemailer/emails.js
+++ backend/nodemailer/emails.js
@@ -42,39 +42,32 @@ export const sendWelcomeEmail = async (email, name) => {
 };
 
 export const sendPasswordResetEmail = async (email, resetURL) => {
-  const recipient = [{ email }];
-
   try {
-    const response = await mailtrapClient.send({
-      from: sender,
-      to: recipient,
+    const response = await transporter.sendMail({
+      from: `"${sender.name}" <${sender.email}>`,
+      to: email,
       subject: "Reset your password",
       html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
-      category: "Password Reset",
     });
+    console.log("Password reset email sent successfully", response.messageId);
   } catch (error) {
     console.error(`Error sending password reset email`, error);
-
     throw new Error(`Error sending password reset email: ${error}`);
   }
 };
 
 export const sendResetSuccessEmail = async (email) => {
-  const recipient = [{ email }];
-
   try {
-    const response = await mailtrapClient.send({
-      from: sender,
-      to: recipient,
+    const response = await transporter.sendMail({
+      from: `"${sender.name}" <${sender.email}>`,
+      to: email,
       subject: "Password Reset Successful",
       html: PASSWORD_RESET_SUCCESS_TEMPLATE,
-      category: "Password Reset",
     });
 
-    console.log("Password reset email sent successfully", response);
+    console.log("Password reset success email sent", response.messageId);
   } catch (error) {
     console.error(`Error sending password reset success email`, error);
-
     throw new Error(`Error sending password reset success email: ${error}`);
   }
 };
